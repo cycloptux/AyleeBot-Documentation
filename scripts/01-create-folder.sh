b41b07e539425594226e5b240a4ae5db4587e75f
cycloptux <ale.impe@gmail.com>
2019-06-30 15:52:00 +0200

    Testing a new deploy script.

--- scripts/01-create-folder.sh
+++ scripts/01-create-folder.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# Create a temporary folder for the build if it doesn't exist
+mkdir -p /tmp/code-deploy
+rm -rf /tmp/code-deploy/*
+
 # Create website folder if it doesn't exist
 mkdir -p /var/www/docs.cycloptux.com/$BOT_IDENTIFIER/public_html
---
+++ scripts/03-move-files.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Move all temporary files to their final directory
+mv /tmp/code-deploy /var/www/docs.cycloptux.com/$BOT_IDENTIFIER/
